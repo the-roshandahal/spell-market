f7fae27ff23bb7b219cd40b077000ccfc08a9eac
Roshan Dahal <58673204+roshan112-rd@users.noreply.github.com>
2022-11-23 17:07:30 +0545

    add template

--- adminpanel/models.py
+++ adminpanel/models.py
@@ -32,18 +32,17 @@ class ChildCategory(models.Model):
 
 class Template(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
-    # child_category = models.ForeignKey(ChildCategory, on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True, blank=True)
+    child_category = models.ForeignKey(ChildCategory, on_delete=models.CASCADE, null=True, blank=True)
     template_name = models.CharField(max_length=1000)
     template_details = models.TextField()
     template_features = models.TextField()
     template_layout = models.TextField()
-    template_price = models.CharField(max_length=100)
-    released_date = models.DateField(auto_now_add=True)
+    template_price = models.IntegerField(max_length=100)
     version = models.CharField(max_length=100)
     framework = models.CharField(max_length=1000)
     template_image = models.ImageField(null=True, upload_to='template_images/' )
-    template_url = models.URLField(default='/')
-
+    template_url = models.URLField( null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
     def __str__(self):
         return self.template_name
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -5,12 +5,14 @@ from .models import *
 
 
 def dashboard(request):
-	return render(request,'admin/dashboard.html')
+    return render(request, 'admin/dashboard.html')
+
 
 def template(request):
-    template= Template.objects.all()
-    
-    return render(request, 'admin/template.html',{'template':template})
+    template = Template.objects.all()
+
+    return render(request, 'admin/template.html', {'template': template})
+
 
 def add_template(request):
 
@@ -26,26 +28,31 @@ def add_template(request):
         template_image = request.FILES['template_image']
         template_url = request.POST['template_url']
 
-        category=Category.objects.get(id = cat_id)
+        category = Category.objects.get(id=cat_id)
 
-        Template.objects.create(category=category, template_name=template_name,template_details=template_details,template_features=template_features,
-        template_layout=template_layout,template_price=template_price,version=version,framework=framework,
-        template_image=template_image,template_url=template_url)
-        
+        Template.objects.create(category=category, template_name=template_name, template_details=template_details, template_features=template_features,
+                                template_layout=template_layout, template_price=template_price, version=version, framework=framework,
+                                template_image=template_image, template_url=template_url)
+
         return redirect('template')
     cat = Category.objects.all()
-    context={
-    'cat':cat
+    sub_cat = SubCategory.objects.all()
+    child_cat = ChildCategory.objects.all()
+    context = {
+        'cat': cat,
+        'sub_cat': sub_cat,
+        'child_cat': child_cat
     }
-    return render(request, 'admin/add_template.html',context)
+    return render(request, 'admin/add_template.html', context)
 
 
 def category(request):
     category = Category.objects.all()
-    context={
-        'category':category
+    context = {
+        'category': category
     }
-    return render(request, 'admin/category.html',context)
+    return render(request, 'admin/category.html', context)
+
 
 def add_category(request):
     if request.method == "POST":
@@ -56,70 +63,70 @@ def add_category(request):
         if Category.objects.filter(category=category).exists():
             messages.info(request, 'the category is already taken')
             return redirect('add_category')
-        Category.objects.create(category=category,order=order, status= status, category_image = category_image)
+        Category.objects.create(
+            category=category, order=order, status=status, category_image=category_image)
         return redirect('category')
     return render(request, 'admin/add_category.html')
 
 
 def sub_category(request):
     sub_category = SubCategory.objects.all()
-    context={
-        'sub_category':sub_category
+    context = {
+        'sub_category': sub_category
     }
-    return render(request, 'admin/sub_category.html',context)
+    return render(request, 'admin/sub_category.html', context)
 
 
 def add_sub_category(request):
     if request.method == "POST":
         cat_id = request.POST['category']
-        sub_category=request.POST['sub_category']
+        sub_category = request.POST['sub_category']
         order = request.POST['order']
         status = request.POST['status']
-
 
         if SubCategory.objects.filter(sub_category=sub_category).exists():
             messages.info(request, 'the category is already taken')
             return redirect('add_sub_category')
-        
-        category=Category.objects.get(id = cat_id)
-        SubCategory.objects.create(category=category,order=order, status= status,sub_category=sub_category)
+
+        category = Category.objects.get(id=cat_id)
+        SubCategory.objects.create(
+            category=category, order=order, status=status, sub_category=sub_category)
         return redirect('sub_category')
     cat = Category.objects.all()
     context = {
-        'cat':cat
+        'cat': cat
     }
-    return render(request, 'admin/add_sub_category.html',context)
-
+    return render(request, 'admin/add_sub_category.html', context)
 
 
 def child_category(request):
     child_category = ChildCategory.objects.all()
-    context={
-        'child_category':child_category
+    context = {
+        'child_category': child_category
     }
-    return render(request, 'admin/child_category.html',context)
+    return render(request, 'admin/child_category.html', context)
 
 
 def add_child_category(request):
     if request.method == "POST":
         sub_cat_id = request.POST['category']
-        child_category=request.POST['child_category']
+        child_category = request.POST['child_category']
         order = request.POST['order']
         status = request.POST['status']
-
 
         if ChildCategory.objects.filter(child_category=child_category).exists():
             messages.info(request, 'the category is already taken')
             return redirect('add_sub_category')
-        
-        sub_category=SubCategory.objects.get(id = sub_cat_id)
-        ChildCategory.objects.create(sub_category=sub_category,order=order, status= status,child_category=child_category)
+
+        sub_category = SubCategory.objects.get(id=sub_cat_id)
+        ChildCategory.objects.create(
+            sub_category=sub_category, order=order, status=status, child_category=child_category)
         return redirect('child_category')
 
     cat = Category.objects.all()
     sub_cat = SubCategory.objects.all()
     context = {
-        'cat':cat,
-        'sub_cat':sub_cat
+        'cat': cat,
+        'sub_cat': sub_cat
     }
-    return render(request, 'admin/add_child_category.html',context)
+    return render(request, 'admin/add_child_category.html', context)
